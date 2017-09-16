29f60288ae807ac9167695889fae513f06aedb94
hema1718 <32011214+hema1718@users.noreply.github.com>
2017-09-16 14:32:26 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-Simport java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 public class Main {
 
@@ -15,7 +15,7 @@ public class Main {
 		      {
 		           String sDate1="1997-08-18";
                        Date date1=new SimpleDateFormat("yyyy-MM-dd").parse(sDate1);
-		           stud[i]=new Student(i+1,"Shravani"+i,date1,8.74+(double)i);
+		           stud[i]=new Student(i+1,"Hema"+i,date1,8.74+(double)i);
 		      }
 		      for(int i=0;i<stud.length;i++)
 		      {
@@ -31,9 +31,9 @@ public class Main {
 		      stud=gp.getStudents();
 		      for(int i=0;i<stud.length;i++)
 		      {
-		           String sDate1="1997-08-18";
+		           String sDate1="1996-08-18";
                        Date date1=new SimpleDateFormat("yyyy-MM-dd").parse(sDate1);
-		           stud[i]=new Student(i+1,"Hema"+i,date1,8.74+(double)i);
+		           stud[i]=new Student(i+1,"swapna"+i,date1,8.74+(double)i);
 		      }
 		      System.out.println();
 		      gp.setStudents(stud);
@@ -46,7 +46,7 @@ public class Main {
 		      System.out.println();
 		      String sDate1="1994-08-18";
                   Date date1=new SimpleDateFormat("yyyy-MM-dd").parse(sDate1);
-		      Student p=new Student(6,"Navya",date1,8.00);
+		      Student p=new Student(6,"sony",date1,8.00);
 		      gp.setStudent(p,2);
 		      for(int i=0;i<stud.length;i++)
 		      {
@@ -55,13 +55,23 @@ public class Main {
 		      System.out.println();
 		      String sDate2="1991-08-18";
                   Date date2=new SimpleDateFormat("yyyy-MM-dd").parse(sDate2);
-		      Student q=new Student(12,"srvs",date2,8.00);
-		      System.out.println("hellllo");
+		      Student q=new Student(12,"ravs",date2,8.00);
+		      //System.out.println("hellllo");
 		      gp.addFirst(q);
-		      for(int i=0;i<stud.length;i++)
+		      Student h[]=gp.getStudents();
+		     for(int i=0;i<h.length;i++)
 		      {
-		            System.out.println(stud[i].getId()+"  "+stud[i].getFullName()+"  "+stud[i].getBirthDate()+"  "+stud[i].getAvgMark());
+		           System.out.println(h[i].getId()+"  "+h[i].getFullName()+"  "+h[i].getBirthDate()+"  "+h[i].getAvgMark());
 		      }
+		      System.out.println();
+		      Student y=new Student(8,"maggie",date2,8.00);
+		      gp.addLast(y);
+		      Student h1[]=gp.getStudents();
+		     for(int i=0;i<h1.length;i++)
+		      {
+		           System.out.println(h1[i].getId()+"  "+h1[i].getFullName()+"  "+h1[i].getBirthDate()+"  "+h1[i].getAvgMark());
+		      }
+		      
 		}
 		catch(IllegalArgumentException e)
 		{
